689cdb27b9efc8e8ee063cd8bb673d7e3e34e69c
kolomyya <44345671+kolomyya@users.noreply.github.com>
2019-01-08 21:01:53 -0600

    Update variables.tf

--- stage/variables.tf
+++ stage/variables.tf
@@ -3,5 +3,5 @@ variable  "ami"  {}
 variable  "instance_type"  {}
 variable  "instance_count"  {}
 variable  "environment"    {}
-variable   "created_by"{}
+variable   "created_by" {}
 variable   "dept"   {}
